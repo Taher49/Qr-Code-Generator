aae18d95c859fbc40e5386a358e7cf1139063ee3
Taher49 <taherelbouhtouri49@gmail.com>
2024-03-03 17:45:22 +0100

    A Simple Qr Code Generator using ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import CodeGenerator from './components/CodeGenerator'
+import './App.css'
+import QRCodeGenerator from './components/CodeGenerator'
+
+function App() {
+
+
+  return (
+    <>
+    <QRCodeGenerator/>
+    </>
+  )
+}
+
+export default App
